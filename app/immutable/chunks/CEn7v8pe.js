c462a9287942495648f9a4a75dfce5a39a44e73e
isanra <ihsanr293@gmail.com>
2026-01-28 00:29:33 +0700

    Updates

--- app/immutable/chunks/CEn7v8pe.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1kb3ha3?.base??"/temani-sehat",a=globalThis.__sveltekit_1kb3ha3?.assets??s??"";export{a,s as b};
---
+++ app/immutable/chunks/DAsZYD6L.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_ki47gc?.base??"/temani-sehat",a=globalThis.__sveltekit_ki47gc?.assets??s??"";export{a,s as b};
